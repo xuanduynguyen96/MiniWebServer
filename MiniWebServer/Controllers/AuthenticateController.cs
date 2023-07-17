79f2fcb6bb08a94885c455078aa95040c7aac70e
xduyng <xuanduynguyen96@gmail.com>
2023-07-17 17:18:10 +0700

    initial version

---
+++ MiniWebServer/Controllers/AuthenticateController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using MiniWebServer.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace MiniWebServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticateController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public AuthenticateController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        [Route("verifytoken")]
+        public IActionResult VerifyToken()
+        {
+            return Ok("The token is still valid");
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("createtoken")]
+        public IActionResult CreateToken(User creadential)
+        {
+            // TODO: Below logic uses a dummy user to create token, consider using a realistic data when running on Production environment
+            if ("testuser".Equals(creadential.Username) && "testpass".Equals(creadential.Password))
+            {
+                // TODO: Once again, these configurations below gotten from json file so consider to replase this code with Options pattern's code, pls looking forward to future commit to get things updated
+                var issuer = _configuration["Jwt:Issuer"];
+                var audience = _configuration["Jwt:Audience"];
+                var key = Encoding.ASCII.GetBytes(_configuration["Jwt:Key"]!); // NOTE: The passed secret key is marked with '!' to make sure that it's not null
+
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(new[] {
+                        new Claim("Id", Guid.NewGuid().ToString()),
+                        new Claim(System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames.Sub, creadential.Username)
+                    }),
+                    Expires = DateTime.UtcNow.AddMinutes(5), // TODO: This expires time is hard code which is not good, make it able to change in future by storing it in a config file
+                    Issuer = issuer,
+                    Audience = audience,
+                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+                };
+
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var token = tokenHandler.CreateToken(tokenDescriptor);
+                var jwtToken = tokenHandler.WriteToken(token);
+                return Ok(jwtToken);
+            }
+            return Unauthorized();
+        }
+    }
+}
---
+++ MiniWebServer/Program.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+// TODO: Wrap these configuration
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        // TODO: Consider using Options pattern to provide a clean and readable way of getting configurations
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!)),
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true
+    };
+});
+
+builder.Services.AddAuthorization();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers()
+   .RequireAuthorization();
+
+app.Run();
